5150ff6da7d4926ea9048ea0d7e913244d2a1e2c
Matias Bacelar <matiasbacelar@gmail.com>
2022-12-24 02:47:31 -0300

    Factory pattern

--- factory-pattern/Info.md
+++ factory-pattern/Info.md
@@ -1,1 +1,5 @@
 # Factory Pattern
+
+The Factory Pattern is useful when we need to create an object multiple times, we can create a function that does this for us.
+
+We can also implement a `factory` which is useful when we need to create different objects (that are related) multiple times.
--- factory-pattern/app.js
+++ factory-pattern/app.js
@@ -1,1 +1,39 @@
-console.log('Factory pattern');
+//----- Basic example -----//
+function createUser(name, age) {
+  return {
+    name: name,
+    age: age,
+    printName() {
+      console.log(this.name);
+    },
+  };
+}
+
+const jackUser = createUser('Jack', 20);
+const roseUser = createUser('Rose', 20);
+
+// jackUser.printName();
+// roseUser.printName();
+
+//------- Factory example -------//
+const factory = {
+  createLocations(defaultLoc, currentLoc) {
+    return {
+      defaultLocation: defaultLoc,
+      currentLocation: currentLoc,
+    };
+  },
+  createFamilyGroup(wife, sons) {
+    return {
+      wife: wife,
+      kids: sons,
+    };
+  },
+};
+
+const infoCard = {
+  locations: factory.createLocations('New York', 'California'),
+  family: factory.createFamilyGroup(true, false),
+};
+
+console.log(infoCard);
