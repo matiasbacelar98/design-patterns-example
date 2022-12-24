2fad5cb617d30d6288f55da2d9cfab36a688af5b
Matias Bacelar <matiasbacelar@gmail.com>
2022-12-24 01:35:13 -0300

    Singleton pattern

--- singleton-pattern/Info.md
+++ singleton-pattern/Info.md
@@ -1,1 +1,19 @@
 # Singleton pattern
+
+The Singleton pattern is a design pattern where the idea is to only have <strong>a Single Object</strong> which state is share across multiple objects.
+
+Can only exist one instance of this object for the whole application.
+
+<img src='https://cdn-media-1.freecodecamp.org/images/1*GOAK3XdRvjrcpX9dq0fUrQ.png' />
+
+#### Pros
+
+- Helps to save memory (only one instance)
+- Only one source of information
+- Can be useful for building loggers, database connections or global configurations
+
+#### Cons
+
+- May introduce unnecessary restrictions
+- Singletons are available anywhere in the app by anything, developers may need to know the inner workings of the singleton
+- May be difficult to test
--- singleton-pattern/app.js
+++ singleton-pattern/app.js
@@ -1,1 +1,42 @@
-console.log('singleton pattern');
+//----- Logger Class -----//
+let instanceWasCreated = false;
+
+class Logger {
+  constructor() {
+    // ! Only allow one instance
+    if (instanceWasCreated) throw new Error('An instance was already created');
+
+    this.logs = [];
+    instanceWasCreated = true;
+  }
+
+  log(message) {
+    this.logs.push(message);
+    console.log(`FANCY: ${message}`);
+  }
+
+  printLogCount() {
+    console.log(`${this.logs.length} Logs`);
+  }
+}
+
+const logger = new Logger();
+Object.freeze(logger);
+
+//----- First File -----//
+function firstFile() {
+  logger.printLogCount();
+  logger.log('First file');
+  logger.printLogCount();
+}
+
+//----- Second File -----//
+function secondFile() {
+  logger.printLogCount();
+  logger.log('Second file');
+  logger.printLogCount();
+}
+
+//----- Initialize -----//
+firstFile();
+secondFile();
