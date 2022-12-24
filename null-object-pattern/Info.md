6f270b315082e4f1902a18215776df426f4feed8
Matias Bacelar <matiasbacelar@gmail.com>
2022-12-24 02:16:49 -0300

    Null object pattern

--- null-object-pattern/Info.md
+++ null-object-pattern/Info.md
@@ -1,1 +1,5 @@
 # Null object pattern
+
+The idea behind this pattern is to create an object instead of manually checking for properties in an object, so by using this pattern we only need to return this `null object` instead of checking for unexisting properties
+
+<img src='https://reactiveprogramming.io/books/patterns/img/patterns-articles/null-object-diagram.png' />
--- null-object-pattern/app.js
+++ null-object-pattern/app.js
@@ -1,1 +1,48 @@
-console.log('Null object pattern');
+function createUser(id, name) {
+  return {
+    name: name,
+    id: id,
+    hasAccess() {
+      return this.name === 'Bob';
+    },
+  };
+}
+
+function createNullUser() {
+  return {
+    name: 'Guest',
+    id: -1,
+    hasAccess() {
+      return false;
+    },
+  };
+}
+
+//--------- Implementation ---------//
+const users = [createUser(1, 'Bob'), createUser(2, 'Jack')];
+
+function getUser(id) {
+  const user = users.find(user => user.id === id);
+
+  // If user was found return
+  if (user) return user;
+
+  // If not user was found then create a 'NULL USER'
+  return createNullUser();
+}
+
+function printUser(id) {
+  const user = getUser(id);
+
+  console.log('Hello ' + user.name);
+
+  if (user.hasAccess()) {
+    console.log('You have access');
+  } else {
+    console.log('You dont have access');
+  }
+}
+
+printUser(3);
+printUser(2);
+printUser(1);
