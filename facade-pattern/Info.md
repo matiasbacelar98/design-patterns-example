4948d07760a3cfe73f22525d4119e6d4d663cc0b
Matias Bacelar <matiasbacelar@gmail.com>
2022-12-24 03:16:09 -0300

    Facade pattern

--- facade-pattern/Info.md
+++ facade-pattern/Info.md
@@ -1,1 +1,3 @@
 # Facade Pattern
+
+This pattern is one of the most used patterns, the fundamental idea behind this pattern is to create code that is ease to replace in the future, the idea is to create a `facade` between the complex code and the business logic code.
--- facade-pattern/app.js
+++ facade-pattern/app.js
@@ -1,1 +1,42 @@
-console.log('facade pattern');
+//------ Without Facade pattern ------//
+// function getUsers() {
+//   return fetch('https://jsonplaceholder.typicode.com/users', {
+//     method: 'GET',
+//     headers: { 'Content-Type': 'application/json' },
+//   }).then(res => res.json());
+// }
+
+// function getUserPosts(userId) {
+//   return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, {
+//     method: 'GET',
+//     headers: { 'Content-Type': 'application/json' },
+//   }).then(res => res.json());
+// }
+
+//------ With Facade pattern ------//
+function getFetch(url) {
+  return fetch(url, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  }).then(res => res.json());
+}
+
+function getUsers() {
+  return getFetch('https://jsonplaceholder.typicode.com/users');
+}
+
+function getUserPosts(userId) {
+  return getFetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+  );
+}
+
+// Print users names and posts amount
+getUsers().then(users => {
+  users.forEach(user => {
+    getUserPosts(user.id).then(posts => {
+      console.log(user.name);
+      console.log(posts.length);
+    });
+  });
+});
